e0db3e33c05551cd2c2576f16f24fd6e39bcc014
vimalraj27 <vimalraj@thirvusoft.in>
2022-03-16 17:15:34 +0530

    Item Doctype Validation Customization

--- ganapathy_pavers/hooks.py
+++ ganapathy_pavers/hooks.py
@@ -89,13 +89,14 @@ app_license = "MIT"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-#	}
-# }
+doc_events = {
+	"Item": {
+		# "on_update": "method",
+		# "on_cancel": "method",
+		# "on_trash": "method",
+		"after_insert": "ganapathy_pavers.ganapathy_pavers.custom.item.multiply"
+	}
+}
 
 # Scheduled Tasks
 # ---------------
