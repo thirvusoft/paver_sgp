353bb3c77a9b70e7eff1fee86c9a61613def50c8
vimalraj27 <vimalraj@thirvusoft.in>
2022-03-17 12:30:33 +0530

    Code modification

--- ganapathy_pavers/ganapathy_pavers/custom/item.py
+++ ganapathy_pavers/ganapathy_pavers/custom/item.py
@@ -3,8 +3,5 @@ import frappe
 import erpnext
 from frappe.model.document import Document
 def multiply(doc,action):
-    print("00000000000000000000000000000000000000000000000000000000000000")
-    print(type(doc.per_plate))
-    print(type(doc.per_rack))
     doc.per_rack=36*doc.per_plate
     doc.pieces_per_bundle=1000/doc.block_weight
