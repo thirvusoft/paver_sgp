219a9038ba551a7240436617ce98e1de8c0e2470
vimalraj27 <vimalraj@thirvusoft.in>
2022-03-24 12:03:05 +0530

    Item Customisation rework

---
+++ ganapathy_pavers/ganapathy_pavers/custom/item.py
@@ -0,0 +1,8 @@
+from time import perf_counter
+import frappe
+import erpnext
+from frappe.model.document import Document
+def multiply(doc,action):
+    return
+    # doc.per_rack=36*doc.per_plate
+    # doc.pieces_per_bundle=1000/doc.block_weight
