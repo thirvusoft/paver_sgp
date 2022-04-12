06a094fe172634b1a401df5e2157a215c733d92f
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2022-04-12 12:33:43 +0530

    Salary slip for timesheet

--- ganapathy_pavers/hooks.py
+++ ganapathy_pavers/hooks.py
@@ -82,9 +82,10 @@ doctype_js = {"Timesheet" : "utils/js/timesheet.js"}
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-# 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+	# "ToDo": "custom_app.overrides.CustomToDo"
+	"Salary Slip":"hrapp.utils.py.salary_slip.CustomSalary"
+}
 
 # Document Events
 # ---------------
