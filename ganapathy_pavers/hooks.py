f1f7f7161d64ff1cad88c3cd15fa6c168c570b7d
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2022-04-21 11:28:41 +0530

    mess-expense-calculated

--- ganapathy_pavers/hooks.py
+++ ganapathy_pavers/hooks.py
@@ -83,7 +83,8 @@ app_license = "MIT"
 
 override_doctype_class = {
 	# "ToDo": "custom_app.overrides.CustomToDo"
-	"Salary Slip":"ganapathy_pavers.utils.py.salary_slip.CustomSalary"
+	"Salary Slip":"ganapathy_pavers.utils.py.salary_slip.CustomSalary",
+	"Payroll Entry":"ganapathy_pavers.utils.py.payroll_entry.MessExpense",
 }
 
 # Document Events
