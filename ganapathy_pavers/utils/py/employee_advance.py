776ae5952e538adf480e68792dd3d79d540fc8bc
MOHAMMEDSAHEETH <saheeth@thirvusoft.in>
2022-04-27 17:30:43 +0530

    employee_advance_updated

--- ganapathy_pavers/utils/py/employee_advance.py
+++ ganapathy_pavers/utils/py/employee_advance.py
@@ -1,4 +1,4 @@
-import frappe
+ import frappe
 def create_payment_entry(doc,action):
         add_doc=frappe.new_doc('Payment Entry')
         add_doc.payment_type='Pay'
@@ -14,10 +14,16 @@ def create_payment_entry(doc,action):
         add_doc.target_exchange_rate=1.0
         add_doc.paid_to=doc.advance_account
         add_doc.paid_from=doc.advance_account
-        add_doc.append('references', {})
-        row.reference_doctype = "Employee Advance"
-        row.reference_name = doc.name
-        row.allocated_amount=doc.paid_amount
+        add_doc.append(
+                "references",
+                {
+                        "reference_doctype": 'Employee Advance',
+                        "reference_name": doc.name,
+                        "total_amount": doc.advance_amount,
+                        "outstanding_amount": doc.advance_amount,
+                        "allocated_amount": doc.advance_amount,
+                },
+        )
         add_doc.insert()
         add_doc.submit()
         frappe.db.commit()
