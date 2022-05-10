f5387c6cc1195819c917d7d593f148172435c961
Ponnusamy1-V <95607086+Ponnusamy1-V@users.noreply.github.com>
2022-05-10 18:49:37 +0530

    Update sales_order.js

--- ganapathy_pavers/custom/js/sales_order.js
+++ ganapathy_pavers/custom/js/sales_order.js
@@ -130,7 +130,7 @@ frappe.ui.form.on('Sales Order',{
                     new_row.description=message['description']
                 }
             })
-            
+            new_row.conversion_factor=1
             new_row.warehouse=cur_frm.doc.set_warehouse
             new_row.delivery_date=cur_frm.doc.delivery_date
             
