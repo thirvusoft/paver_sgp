3df5f039accb175c3d90ad86352d450d01ef9bd4
Nithya Swaminathan <nithya@thirvusoft.in>
2022-06-07 11:41:42 +0530

    commit

--- ganapathy_pavers/custom/py/workstation.py
+++ ganapathy_pavers/custom/py/workstation.py
@@ -8,5 +8,7 @@ def hour_salary_finder(ts_employee):
         ts_structure_name=frappe.get_all("Salary Structure Assignment",filters={"employee":ts_employee},fields={"salary_structure"})
         ts_structure_details=frappe.get_doc("Salary Structure",ts_structure_name[0]["salary_structure"])
         ts_emp_wages_per_hour=ts_emp_wages_per_hour+ts_structure_details.__dict__["hour_rate"]
-    ts_avg_wages_per_hour=ts_emp_wages_per_hour/len(ts_employee_list)
-    return ts_avg_wages_per_hour
+    # ts_avg_wages_per_hour=ts_emp_wages_per_hour/len(ts_employee_list)
+    # return ts_avg_wages_per_hour
+    return ts_emp_wages_per_hour
+    
