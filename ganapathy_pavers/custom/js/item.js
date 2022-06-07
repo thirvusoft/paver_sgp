d73957572097de6e14112b9aa3e9d52f92acd5bf
sibikumarkuppusamy <sibi.kumar@thirvusoft.in>
2022-06-07 14:04:51 +0530

    changes in compound walls

--- ganapathy_pavers/custom/js/item.js
+++ ganapathy_pavers/custom/js/item.js
@@ -11,6 +11,12 @@ frappe.ui.form.on("Item", {
     weight_per_paver:function(frm,cdt,cdn){
         bundle(frm,cdt,cdn)
     },
+    weight_per_piece : function(frm,cdt,cdn) {
+        bundle(frm,cdt,cdn)
+    },
+    pieces_per_bundle : function(frm,cdt,cdn) {
+        bundle(frm,cdt,cdn)
+    },
     no_of_layers_per_bundle:function(frm,cdt,cdn){
         var data = locals[cdt][cdn]
         pavers_per_bundle = data.pavers_per_layer *data.no_of_layers_per_bundle
@@ -34,6 +40,9 @@ function bundle(frm,cdt,cdn) {
     var data = locals[cdt][cdn]
     var weight_per_paver = data.weight_per_paver
     var weight_per_bundle = data.pavers_per_bundle * weight_per_paver
+    if(data.weight_per_piece > 0 && data.pieces_per_bundle > 0){
+        weight_per_bundle = data.pieces_per_bundle * data.weight_per_piece
+    }
     frm.set_value("weight_per_bundle",weight_per_bundle.toFixed(2))
     var bundle_per_sqr_ft = data.pavers_per_bundle/data.pavers_per_sqft
     frm.set_value("bundle_per_sqr_ft" , bundle_per_sqr_ft.toFixed(2))
