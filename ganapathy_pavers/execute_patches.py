1b54bdb1718326b7358472dc08f5cf2ffd8d97a4
vimalraj27 <vimalraj@thirvusoft.in>
2022-07-19 10:33:20 +0530

    Patches with some minor customization

---
+++ ganapathy_pavers/execute_patches.py
@@ -0,0 +1,8 @@
+from ganapathy_pavers.utils.py.vehicle_log import batch_customization as a
+from ganapathy_pavers.utils.py.vehicle import batch_customization as b
+from  ganapathy_pavers.utils.py.maintenance_details import batch_customization as c
+
+def execute():
+    a()
+    b()
+    c()
