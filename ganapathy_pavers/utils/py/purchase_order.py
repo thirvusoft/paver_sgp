9ff113416cdca551c956a60ac3e61b5c83c2456f
vimalraj27 <vimalraj@thirvusoft.in>
2022-07-20 15:52:17 +0530

    Purchase Order Notification

--- ganapathy_pavers/utils/py/purchase_order.py
+++ ganapathy_pavers/utils/py/purchase_order.py
@@ -1,6 +1,8 @@
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from frappe.custom.doctype.property_setter.property_setter import make_property_setter
 def batch_customization():
     batch_custom_fields()
+    batch_property_setter()
 def batch_custom_fields():
     custom_fields = {
         "Purchase Order": [
@@ -15,3 +17,6 @@ def batch_custom_fields():
         ]
     }
     create_custom_fields(custom_fields)
+
+def batch_property_setter():                
+    make_property_setter("Purchase Order", "schedule_date", "allow_on_submit", "0", "Check")
