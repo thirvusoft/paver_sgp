44709a0ea6d951937527b01573b19ecef17cbe97
Barathpalanisamy <barath@thirvusoft.in>
2022-07-22 17:43:25 +0530

    Vehicle Settings Created

--- ganapathy_pavers/custom/js/vehicle_log.js
+++ ganapathy_pavers/custom/js/vehicle_log.js
@@ -8,8 +8,7 @@ frappe.ui.form.on("Vehicle Log" ,{
                                 'designation': 'Driver'
                             }
                         }
-                    });
-                    
+                    });                
     },
     odometer: function(frm){
         distance(frm)
@@ -23,7 +22,6 @@ frappe.ui.form.on("Vehicle Log" ,{
         total_cost(frm)
     },
     driver_cost: function(frm){
-        console.log(from_lp)
         if(from_lp){
             total_cost(frm)
         }
@@ -44,19 +42,36 @@ frappe.ui.form.on("Vehicle Log" ,{
 function distance(frm){
     frm.set_value('today_odometer_value', (frm.doc.odometer?frm.doc.odometer:0) - (frm.doc.last_odometer?frm.doc.last_odometer:0))
 }
-
 function total_cost(frm){
     if(frm.doc.license_plate){
         frappe.db.get_doc('Vehicle', frm.doc.license_plate).then( async(doc) => {
-            let fuel_cost_per_km=(doc.fuel_cost_per_km?doc.fuel_cost_per_km:zero_alert('Fuel Cost'))*(frm.doc.today_odometer_value?frm.doc.today_odometer_value:0)/(doc.mileage?doc.mileage:zero_alert('Mileage'));
+            let fuel_cost=0
+            if(doc.fuel_type=='Petrol'){
+                await frappe.db.get_single_value("Vehicle Settings","petrol_per_liter").then((i)=> {fuel_cost=i})
+                if(fuel_cost==0){
+                    zero_alert('Petrol Cost', 'vehicle-settings', 'Vehicle Settings')
+                }
+            }
+            else if(doc.fuel_type=='Diesel'){
+                await frappe.db.get_single_value("Vehicle Settings","diesel_per_liter").then((i)=> {fuel_cost=i})
+                if(fuel_cost==0){
+                    zero_alert('Diesel Cost', 'vehicle-settings', 'Vehicle Settings')
+                }
+            }
+            else{
+                frappe.show_alert({message: 'Please Choose Petrol or Diesel as Fuel Type for '+frappe.utils.get_form_link('Vehicle', cur_frm.doc.license_plate, cur_frm.doc.license_plate)+'..!', indicator: 'red'})
+            }
+            
+            let fuel_cost_per_km=(fuel_cost?fuel_cost:0)*(frm.doc.today_odometer_value?frm.doc.today_odometer_value:0)/(doc.mileage?doc.mileage:zero_alert('Mileage'));
             let mc=(frm.doc.today_odometer_value?frm.doc.today_odometer_value:0)*(doc.maintenance_cost?doc.maintenance_cost:zero_alert('Maintenance Cost'))
             let dc=(frm.doc.today_odometer_value?frm.doc.today_odometer_value:0)*(frm.doc.driver_cost?frm.doc.driver_cost:0)
             frm.set_value('ts_total_cost', (fuel_cost_per_km?fuel_cost_per_km:0)+(mc?mc:0)+(dc?dc:0))
             frm.refresh()
         })
+
     }
 }
-function zero_alert(field){
-    frappe.show_alert({message: field+' is Empty in '+frappe.utils.get_form_link('Vehicle', cur_frm.doc.license_plate, cur_frm.doc.license_plate)+'..!', indicator: 'red'})
+function zero_alert(field,vehicle="Vehicle", lp=cur_frm.doc.license_plate, name=cur_frm.doc.license_plate){
+    frappe.show_alert({message: field+' is Empty in '+frappe.utils.get_form_link(vehicle, lp, name)+'..!', indicator: 'red'})
     return 0
-}    
+}
