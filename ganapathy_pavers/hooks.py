948456af7954790221374e5d975df6b32f2cf089
vimalraj27 <vimalraj@thirvusoft.in>
2022-07-26 16:45:26 +0530

    Item field customizations using python

--- ganapathy_pavers/hooks.py
+++ ganapathy_pavers/hooks.py
@@ -68,7 +68,8 @@ after_install = ["ganapathy_pavers.custom.py.item_group.item_group",
 				 "ganapathy_pavers.utils.py.vehicle_log.batch_customization",
 				 "ganapathy_pavers.utils.py.assets.item_customization",
 				 "ganapathy_pavers.utils.py.worstation.item_customization",
-				 "ganapathy_pavers.utils.py.purchase_order.batch_customization"
+				 "ganapathy_pavers.utils.py.purchase_order.batch_customization",
+				 "ganapathy_pavers.utils.py.item.batch_customization"
 				 ]
 				
 
@@ -194,7 +195,8 @@ after_migrate=["ganapathy_pavers.custom.py.site_work.create_status",
               "ganapathy_pavers.custom.py.property_setter.property_setter",
 			  "ganapathy_pavers.utils.py.vehicle.batch_customization",
 			  "ganapathy_pavers.utils.py.maintenance_details.batch_customization",
-			  "ganapathy_pavers.utils.py.vehicle_log.batch_customization"]
+			  "ganapathy_pavers.utils.py.vehicle_log.batch_customization",
+			  "ganapathy_pavers.utils.py.item.batch_customization"]
 
 
 doctype_js = {
