526717fb4d033ff6c5eb448dbd4dda1b4eec673e
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2022-08-02 18:22:33 +0530

    chore: Minor Changes

--- ganapathy_pavers/custom/py/workstation.py
+++ ganapathy_pavers/custom/py/workstation.py
@@ -24,32 +24,32 @@ def validate(doc, action):
         else:
              doc.cal_wages1 = 0
              doc.division_factors1 = 0
-        if doc.no_of_operators !=0:
-            if doc.division_factors2 == 0:
-                frappe.throw("Zero is not allowed kindly give above zero in division factors")
-            else:
-                doc.cal_wages2 = ((salary or 0)/doc.division_factors2 or 0)*doc.no_of_operators
-                
+    if doc.no_of_operators !=0:
+        if doc.division_factors2 == 0:
+            frappe.throw("Zero is not allowed kindly give above zero in division factors")
         else:
-            doc.cal_wages2 = 0
-            doc.division_factors2 = 0
-        if doc.no_of_common_operators !=0:
-            if doc.division_factors3 == 0:
-                frappe.throw("Zero is not allowed kindly give above zero in division factors")
-            else:
-                doc.cal_wages3 = ((salary or 0)/doc.division_factors3 or 0)*doc.no_of_common_operators    
+            doc.cal_wages2 = ((salary or 0)/doc.division_factors2 or 0)*doc.no_of_operators
+            
+    else:
+        doc.cal_wages2 = 0
+        doc.division_factors2 = 0
+    if doc.no_of_common_operators !=0:
+        if doc.division_factors3 == 0:
+            frappe.throw("Zero is not allowed kindly give above zero in division factors")
         else:
-            doc.cal_wages3 = 0
-            doc.division_factors3 = 0
-        if doc.routine_operators !=0:
-            if doc.division_factors4 == 0:
-                frappe.throw("Zero is not allowed kindly give above zero in division factors")
-            else:
-                doc.cal_wages4 = ((salary or 0)/doc.division_factors4 or 0)*doc.routine_operators
-               
+            doc.cal_wages3 = ((salary or 0)/doc.division_factors3 or 0)*doc.no_of_common_operators    
+    else:
+        doc.cal_wages3 = 0
+        doc.division_factors3 = 0
+    if doc.routine_operators !=0:
+        if doc.division_factors4 == 0:
+            frappe.throw("Zero is not allowed kindly give above zero in division factors")
         else:
-            doc.cal_wages4 = 0
-            doc.division_factors4 = 0
+            doc.cal_wages4 = ((salary or 0)/doc.division_factors4 or 0)*doc.routine_operators
+            
+    else:
+        doc.cal_wages4 = 0
+        doc.division_factors4 = 0
             
         doc.no_of_total_employees = (doc.no_of_labours or 0) + (doc.no_of_operators or 0) + (doc.no_of_common_operators or 0) + (doc.routine_operators or 0)
         doc.sum_of_wages_per_hours = (doc.cal_wages1 or 0) + (doc.cal_wages2 or 0) + (doc.cal_wages3 or 0) + (doc.cal_wages4 or 0)
