02f9233412e8505514d64a00456f3f8d85d96781
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2022-08-02 11:58:34 +0530

    feat: Workstation

--- ganapathy_pavers/custom/py/workstation.py
+++ ganapathy_pavers/custom/py/workstation.py
@@ -1,3 +1,4 @@
+from ast import operator
 import frappe
 
 def validate(doc, action):
@@ -11,13 +12,24 @@ def validate(doc, action):
     #     else:no_salary.append(emp)
     # if(len(no_salary)):
     #     frappe.throw("Salary Structure Assignment not found for employees "+frappe.bold(", ".join(list(set(no_salary)))))
+    
+    # doc.cal_wages3 = salary/doc.division_factors3
+    # doc.cal_wages4 = salary/doc.division_factors4
     if doc.cost_per_hours != 0:
-        if (doc.division_factors1 and doc.division_factors2 and doc.division_factors3 )== 0:
-            frappe.throw("Zero is not allowed kindly give above zero in division factors")
-        else:
-            doc.cal_wages1 = ((doc.cost_per_hours or 0 )* (doc.no_of_labours or 0)) / (doc.division_factors1 or 1)
-            doc.cal_wages2 = ((doc.cost_per_hours or 0) * (doc.no_of_operators or 0)) / (doc.division_factors2 or 1)
-            doc.cal_wages3 = ((doc.cost_per_hours or 0) * (doc.no_of_common_operators or 0) ) / (doc.division_factors3 or 1)
-            doc.no_of_total_employees = (doc.no_of_labours or 0) + (doc.no_of_operators or 0) + (doc.no_of_common_operators or 0)
-            doc.sum_of_wages_per_hours = (doc.cal_wages1 or 0) + (doc.cal_wages2 or 0) + (doc.cal_wages3 or 0)
-
+        if (doc.no_of_operators and doc.no_of_common_operators and doc.routine_operators) != 0:
+            if (doc.division_factors1 and doc.division_factors2 and doc.division_factors3 and doc.division_factors4)== 0:
+                frappe.throw("Zero is not allowed kindly give above zero in division factors")
+            else:
+                doc.cal_wages1 = ((doc.cost_per_hours or 0 )* (doc.no_of_labours or 0)) / (doc.division_factors1 or 1)
+                emp = frappe.get_all("Employee",filters={"designation":'Operator'}, pluck='name')
+                salary = sum(frappe.get_all("Salary Structure Assignment", filters={'employee':['in',emp]}, pluck='base')) / 26
+                print(salary)
+                doc.cal_wages2 = ((salary or 0)/doc.division_factors2 or 0)*doc.no_of_operators 
+                doc.cal_wages3 = ((salary or 0)/doc.division_factors3 or 0)*doc.no_of_common_operators
+                doc.cal_wages4 = ((salary or 0)/doc.division_factors4 or 0)*doc.routine_operators
+                
+                # doc.cal_wages2 = ((doc.cost_per_hours or 0) * (doc.no_of_operators or 0)) / (doc.division_factors2 or 1)
+                # doc.cal_wages3 = ((doc.cost_per_hours or 0) * (doc.no_of_common_operators or 0) ) / (doc.division_factors3 or 1)
+                doc.no_of_total_employees = (doc.no_of_labours or 0) + (doc.no_of_operators or 0) + (doc.no_of_common_operators or 0) + (doc.routine_operators or 0)
+                doc.sum_of_wages_per_hours = (doc.cal_wages1 or 0) + (doc.cal_wages2 or 0) + (doc.cal_wages3 or 0) + (doc.cal_wages4 or 0)
+    
