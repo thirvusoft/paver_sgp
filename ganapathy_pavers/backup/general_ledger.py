d1106cab31736aba6582143b2ec6007e12c51fdb
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-09-05 08:52:09 +0000

    live_changes

--- ganapathy_pavers/backup/general_ledger.py
+++ ganapathy_pavers/backup/general_ledger.py
@@ -1,7 +1,23 @@
 # General Entry Backup
-# Line  229 to 230
+# Line  160 to 180
 def make_entry(args, adv_adj, update_outstanding, from_repost=False):
-    # 229
-    if gle.voucher_type in ['Payment Entry','Sales Invoice','Sales Order','Journal Entry'] and frappe.db.get_value(gle.voucher_type,gle.voucher_no,'type'):
+	gle = frappe.new_doc("GL Entry")
+	try:
+		if gle.voucher_type in ['Payment Entry','Sales Invoice','Sales Order','Journal Entry'] and frappe.db.get_value(gle.voucher_type,gle.voucher_no,'type'):
 			gle.update({'type':frappe.db.get_value(gle.voucher_type,gle.voucher_no,'type')})
-    # 230
+		if gle.voucher_type == "Delivery Note":
+			gle.update({
+				'party': frappe.db.get_value(gle.voucher_type,gle.voucher_no,'customer'),
+				'party_type': "Customer"		
+				})
+	except:
+		pass
+	gle.update(args)
+	gle.flags.ignore_permissions = 1
+	gle.flags.from_repost = from_repost
+	gle.flags.adv_adj = adv_adj
+	gle.flags.update_outstanding = update_outstanding or 'Yes'
+	gle.submit()
+
+	if not from_repost:
+		validate_expense_against_budget(args)
