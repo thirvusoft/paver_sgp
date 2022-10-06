faf61626e60fd59dc27c6c5973bfe4e48c839710
NaviN <navin@thirvusoft.in>
2022-10-06 18:23:14 +0530

    field customization payment entry

--- ganapathy_pavers/hooks.py
+++ ganapathy_pavers/hooks.py
@@ -74,7 +74,8 @@ after_install = ["ganapathy_pavers.custom.py.item_group.item_group",
 				 "ganapathy_pavers.utils.py.customer.create_multi_customer",
 				 "ganapathy_pavers.utils.py.item.batch_customization",
 				 "ganapathy_pavers.patches.location.execute",
-				 "ganapathy_pavers.utils.py.quotation.batch_property_setter"
+				 "ganapathy_pavers.utils.py.quotation.batch_property_setter",
+				 "ganapathy_pavers.utils.py.payment_entry.payment_entry_property_setter"
 				 ]
 				
 
