b438e9e93fc7ed157f27b3c97bef5ccdeabf94d6
Vimalraj R <vimalraj@thirvusoft.in>
2022-10-07 15:17:25 +0530

    Update maintenance_details.py

--- ganapathy_pavers/utils/py/maintenance_details.py
+++ ganapathy_pavers/utils/py/maintenance_details.py
@@ -1,8 +1,6 @@
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
-from frappe.custom.doctype.property_setter.property_setter import make_property_setter
 def batch_customization():
     batch_custom_fields()
-    batch_property_setter()
 def batch_custom_fields():
     custom_fields = {
         "Maintenance Details": [
@@ -15,6 +13,3 @@ def batch_custom_fields():
             ]
     }
     create_custom_fields(custom_fields)
-    
-def batch_property_setter():                
-    make_property_setter("Vehicle", "add_on_details", "hidden", "1", "Check")
