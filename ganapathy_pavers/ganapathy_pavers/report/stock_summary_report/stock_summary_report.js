752521305d2d21fbaf5e7d9ecaff09d9f4e77f25
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-11-09 13:56:58 +0000

    Stock Summary Report with filters

---
+++ ganapathy_pavers/ganapathy_pavers/report/stock_summary_report/stock_summary_report.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Stock Summary Report"] = {
+	"filters": [
+		{
+			"fieldname":"group_by",
+			"label": __("Group By"),
+			"fieldtype": "Select",
+			"width": "80",
+			"reqd": 1,
+			"options": ["Warehouse", "Company", "Item Group"],
+			"default": "Item Group",
+		},
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Company",
+			"reqd": 1,
+			"default": frappe.defaults.get_user_default("Company"),
+			"depends_on": "eval: doc.group_by == 'Company'",
+		},
+		{
+			"fieldname": "item_group",
+			"label": __("Item Group"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Item Group",
+			"depends_on": "eval: doc.group_by == 'Item Group'",
+		},
+	]
+}
