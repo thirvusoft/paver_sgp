d632b7aba3cccbd99903608d70a010c69d1345bd
ChandanaSPthirvusoft <chandana@thirvusoft.in>
2022-11-14 18:09:09 +0530

    Monthly compound wall production Report

---
+++ ganapathy_pavers/ganapathy_pavers/report/monthly_compound_wall_production_report/monthly_compound_wall_production_report.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2022, Thirvusoft and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Monthly Compound Wall Production Report"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"width": "80"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"width": "80"
+		}
+	]
+ };
+ 
