4d11b49d59e2f85404c7769573b4b677e5ff24cf
Barathpalanisamy <barath@thirvusoft.in>
2022-11-15 17:49:15 +0530

    vehicle details report

---
+++ ganapathy_pavers/ganapathy_pavers/report/monthly_transport_report/monthly_transport_report.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2022, Thirvusoft and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Monthly Transport Report"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_days(frappe.datetime.get_today(), -7),
+			"width": "80"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"width": "80"
+		},
+		{
+			"fieldname":"vehicle_no",
+			"label": __("Vehicle No"),
+			"fieldtype": "Link",
+			"options": "Vehicle",
+			"width": "100"
+		},
+
+	]
+};
