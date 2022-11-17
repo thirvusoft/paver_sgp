81881da28635a2ac85641fa017d34e8b422d528c
Chandana S P <95605193+ChandanaSPthirvusoft@users.noreply.github.com>
2022-11-17 16:01:34 +0530

    Update monthly_transport_report.js

--- ganapathy_pavers/ganapathy_pavers/report/monthly_transport_report/monthly_transport_report.js
+++ ganapathy_pavers/ganapathy_pavers/report/monthly_transport_report/monthly_transport_report.js
@@ -9,28 +9,32 @@ frappe.query_reports["Monthly Transport Report"] = {
 			"label": __("From Date"),
 			"fieldtype": "Date",
 			"default": frappe.datetime.add_days(frappe.datetime.get_today(), -7),
-			"width": "80"
+			"width": "80",
+			"reqd":1
 		},
 		{
 			"fieldname":"to_date",
 			"label": __("To Date"),
 			"fieldtype": "Date",
 			"default": frappe.datetime.get_today(),
-			"width": "80"
+			"width": "80",
+			"reqd":1
 		},
 		{
 			"fieldname":"vehicle_no",
 			"label": __("Vehicle No"),
 			"fieldtype": "Link",
 			"options": "Vehicle",
-			"width": "100"
+			"width": "100",
+			"reqd":1
 		},
 		{
 			"fieldname":"transport_based_on",
 			"label": __("Monthly Transport Based On"),
 			"fieldtype": "Select",
 			"options": "Report\nSummary",
-			"width": "200"
+			"width": "200",
+			"default":"Report"
 		},
 	]
 };
