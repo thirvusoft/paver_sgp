3af1a0dc18a06bad5c55fed78a294c52a89b4055
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-11-18 11:25:40 +0000

    Payment entry type field customization & Site Work print format: expenses grouped

--- ganapathy_pavers/utils/py/sitework_printformat.py
+++ ganapathy_pavers/utils/py/sitework_printformat.py
@@ -3,6 +3,7 @@ from ganapathy_pavers.__init__ import uom_conversion
 def site_work(doc):
     doc=frappe.get_doc("Project",doc)
     items={}
+    exp={}
     nos=0
     sqf=0
     for i in doc.delivery_detail:
@@ -14,7 +15,14 @@ def site_work(doc):
         nos+=uom_conversion(i.item,i.stock_uom,i.delivered_stock_qty,"Nos")
         items[i.item]["sqf"]+=round(uom_conversion(i.item,i.stock_uom,i.delivered_stock_qty,"SQF"),2)
         sqf+=uom_conversion(i.item,i.stock_uom,i.delivered_stock_qty,"SQF")
-    return {'items':items,'nos':round(nos,2),'sqf':round(sqf,2)}
+    for row in doc.additional_cost:
+        if row.description.lower().strip() not in exp:
+            exp[row.description.lower().strip()]={"description": row.description, "nos": row.nos, "amount": row.amount, "sqft_amount": (row.amount or 0)/sqf}
+        else:
+            exp[row.description.lower().strip()]["nos"]+=row.nos
+            exp[row.description.lower().strip()]["amount"]+=row.amount
+            exp[row.description.lower().strip()]["sqft_amount"]+=(row.amount or 0)/sqf
+    return {'items':items,'nos':round(nos,2),'sqf':round(sqf,2), 'expense': list(exp.values())}
     
 
 
