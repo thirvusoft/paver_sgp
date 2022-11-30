3b95206dc7a1f70ebcffddbf842725cd06fe68c9
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-11-30 19:48:09 +0530

    Production Details Dashboard

--- ganapathy_pavers/custom/js/journal_entry.js
+++ ganapathy_pavers/custom/js/journal_entry.js
@@ -1,7 +1,73 @@
 frappe.ui.form.on("Journal Entry", {
+    refresh: function (frm) {
+        dashboard_data(cur_frm.doc.posting_date)
+    },
+    posting_date: function (frm) {
+        dashboard_data(cur_frm.doc.posting_date)
+    },
     onload_post_render: function (frm) {
         if (frm.is_new()) {
             frm.set_value("branch", "");
         }
     }
 });
+
+function dashboard_data(date) {
+    if(!date) {
+        cur_frm.dashboard.clear_comment()
+        return
+    }
+    var month, paver, cw, lego, fp;
+    frappe.call({
+        method: "ganapathy_pavers.custom.py.journal_entry.get_production_details",
+        args: {
+            date: date
+        },
+        callback(r) {
+            let res = r.message;
+            month = res.month || "";
+            paver = res.paver || 0;
+            cw = res.cw || 0;
+            lego = res.lego || 0;
+            fp = res.fp || 0;
+            let msg = `
+                <div class="production-heading">
+                    PRODUCTION DETAILS OF ${month.toUpperCase()} MONTH
+                </div>
+                <div class="production-info">
+                    <div><li>Paver: ${parseInt(paver)}</li></div>
+                    <div><li>Compound Wall: ${parseInt(cw)}</li></div>
+                    <div><li>Lego Block: ${parseInt(lego)}</li></div>
+                    <div><li>Fencing Post: ${parseInt(fp)}</li></div>
+                </div>
+                <style>
+                    .production-heading {
+                        width: 100%;
+                        font-size: 130%;
+                        text-align: center;
+                        font-weight: bold;
+                        margin-bottom: 3mm;
+                    }
+                    .production-info {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        width: 100%;
+                        gap: 20px;
+                        flex-wrap: nowrap;
+                    }
+                    .production-info div {
+                        font-size: 110%;
+                        padding: 10px;
+                        width: 25%;
+                        background: rgb(91 86 86 / 50%);
+                        border-radius: 10px;
+                        color: rgb(0 0 0);
+                    }
+                </style>
+            `
+            cur_frm.dashboard.clear_comment()
+            cur_frm.dashboard.add_comment(msg, 'yellow', 1)
+        }
+    });
+}
--- ganapathy_pavers/custom/py/journal_entry.py
+++ ganapathy_pavers/custom/py/journal_entry.py
@@ -1,4 +1,10 @@
 import frappe
+from datetime import datetime
+from ganapathy_pavers import uom_conversion
+from frappe.utils.data import getdate
+from dateutil.relativedelta import relativedelta
+
+
 def journal_entry(self, event):
     for i in self.accounts:
         if i.party and i.party_type :
@@ -7,3 +13,39 @@ def journal_entry(self, event):
                 self.party_address=address
             self.party_name=i.party
             break
+
+@frappe.whitelist()
+def get_production_details(date):
+    res={'month': '', 'paver': 0, 'cw': 0, 'lego': 0, 'fp': 0}
+    try:
+        date=datetime.strptime(date, "%Y-%m-%d")
+        to_date=date+relativedelta(day=1, months=+1, days=-1)
+        from_date=date+relativedelta(day=1)
+        res['month']=date.strftime("%B")
+        query=f"""
+            select sed.item_code, sum(sed.transfer_qty) as qty, sed.stock_uom as uom from `tabStock Entry Detail` as sed 
+            left outer join `tabStock Entry` as se on se.name=sed.parent where se.docstatus=1 and 
+            se.stock_entry_type='Manufacture' and sed.is_finished_item=1 and
+            se.posting_date >= "{from_date}" and se.posting_date <= "{to_date}"
+            group by sed.item_code, sed.uom;
+
+        """
+        query_res=frappe.db.sql(query, as_dict=True)
+        for data in query_res:
+            item_group=frappe.db.get_value("Item", data['item_code'], 'item_group')
+            if item_group=="Pavers":
+                sqf=uom_conversion(data['item_code'], data['uom'], data['qty'], 'SQF')
+                res['paver']+=(sqf or 0)
+            elif item_group=="Compound Walls":
+                _type=frappe.db.get_value("Item", data["item_code"], 'compound_wall_type')
+                sqf=uom_conversion(data['item_code'], data['uom'], data['qty'], 'SQF')
+                if _type in ["Post", "Slab"]:
+                    res['cw']+=(sqf or 0)
+                elif _type=="Fencing Post":
+                    res['fp']+=(sqf or 0)
+                elif _type=="Lego Block":
+                    res['lego']+=(sqf or 0)
+    except:
+        pass
+    return res
+
