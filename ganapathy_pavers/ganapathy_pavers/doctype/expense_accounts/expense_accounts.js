6c1e35930fbb8e54bda85b5364ea545001400004
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-12-03 09:11:58 +0000

    Changes

--- ganapathy_pavers/ganapathy_pavers/doctype/expense_accounts/expense_accounts.js
+++ ganapathy_pavers/ganapathy_pavers/doctype/expense_accounts/expense_accounts.js
@@ -12,37 +12,37 @@ frappe.ui.form.on('Expense Accounts', {
 			}
 		}));
 
-		frm.set_query("paver_account", "expense_account_common_groups", function() {
-			return {
-				filters: {
-					parent_account: cur_frm.doc.paver_group,
-					is_group: 0
-				}
-			}
-		});
-		frm.set_query("cw_account", "expense_account_common_groups", function() {
-			return {
-				filters: {
-					parent_account: cur_frm.doc.cw_group,
-					is_group: 0
-				}
-			}
-		});
-		frm.set_query("lg_account", "expense_account_common_groups", function() {
-			return {
-				filters: {
-					parent_account: cur_frm.doc.lg_group,
-					is_group: 0
-				}
-			}
-		});
-		frm.set_query("fp_account", "expense_account_common_groups", function() {
-			return {
-				filters: {
-					parent_account: cur_frm.doc.fp_group,
-					is_group: 0
-				}
-			}
-		});
+		// frm.set_query("paver_account", "expense_account_common_groups", function() {
+		// 	return {
+		// 		filters: {
+		// 			parent_account: cur_frm.doc.paver_group,
+		// 			is_group: 0
+		// 		}
+		// 	}
+		// });
+		// frm.set_query("cw_account", "expense_account_common_groups", function() {
+		// 	return {
+		// 		filters: {
+		// 			parent_account: cur_frm.doc.cw_group,
+		// 			is_group: 0
+		// 		}
+		// 	}
+		// });
+		// frm.set_query("lg_account", "expense_account_common_groups", function() {
+		// 	return {
+		// 		filters: {
+		// 			parent_account: cur_frm.doc.lg_group,
+		// 			is_group: 0
+		// 		}
+		// 	}
+		// });
+		// frm.set_query("fp_account", "expense_account_common_groups", function() {
+		// 	return {
+		// 		filters: {
+		// 			parent_account: cur_frm.doc.fp_group,
+		// 			is_group: 0
+		// 		}
+		// 	}
+		// });
 	}
 });
