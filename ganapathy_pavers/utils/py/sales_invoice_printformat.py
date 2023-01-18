caa64cd36e615ccb5ece50ce4f06cb2c02d87e67
Nandhinidevi123 <nandhini.devi@thirvusoft.in>
2023-01-18 18:40:13 +0530

    sales invoice print format customized

--- ganapathy_pavers/utils/py/sales_invoice_printformat.py
+++ ganapathy_pavers/utils/py/sales_invoice_printformat.py
@@ -2,6 +2,41 @@
 import frappe
 
 def sales_invoice_print_format(docname):
-    sales_item=  frappe.db.sql("""select si_item.item_name as item_name,si_item.print_item_name as print_item_name , sum(si_item.qty) as qty, sum(si_item.rate) as rate, sum(si_item.amount) as amount,si_item.gst_hsn_code as hsn_code,si_item.uom as uom from `tabSales Invoice Item` as si_item where parent=%s group by si_item.print_item_name """,docname,as_dict=1)
-    print(sales_item)
-    return sales_item
+    data={}
+    sales_doc=frappe.db.get_all('Sales Invoice Item', filters={'parent':docname}, fields=['print_item_name','item_name',"gst_hsn_code","qty","uom","rate","amount"])
+       
+    for row in sales_doc:
+        if row.print_item_name:
+            if f"""{row.get("print_item_name")} {row.get("rate")}  {row.get("uom")}""" not in data:
+                data[f"""{row.get("print_item_name")} {row.get("rate")}  {row.get("uom")}"""]={
+                "item_name": row.get("print_item_name"),
+                "uom": row.get("uom"),
+                "rate": row.get("rate"),
+                "hsn_code":row.get("gst_hsn_code"),
+                "amount": row.get("amount"),
+                "qty": row.get("qty")
+            }
+            else:
+                data[f"""{row.get("print_item_name")} {row.get("rate")}  {row.get("uom")}"""]["amount"] += row.get("amount")
+                data[f"""{row.get("print_item_name")} {row.get("rate")}  {row.get("uom")}"""]["qty"] += row.get("qty")
+        else:
+            if f"""{row.get("item_name")} {row.get("rate")}  {row.get("uom")}""" not in data:
+                data[f"""{row.get("item_name")} {row.get("rate")}  {row.get("uom")}"""]={
+                "item_name": row.get("item_name"),
+                "uom": row.get("uom"),
+                "rate": row.get("rate"),
+                "hsn_code":row.get("gst_hsn_code"),
+                "amount": row.get("amount"),
+                "qty": row.get("qty")
+            }
+            else:
+                data[f"""{row.get("item_name")} {row.get("rate")}  {row.get("uom")}"""]["amount"] += row.get("amount")
+                data[f"""{row.get("item_name")} {row.get("rate")}  {row.get("uom")}"""]["qty"] += row.get("qty")
+
+    data=list(data.values())
+    return data
+  
+
+
+    
+    
