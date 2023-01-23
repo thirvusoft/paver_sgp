11d4975201b6820dcd08ba841341253462889376
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2023-01-23 13:10:07 +0530

    Item Wise Monthly CW Production Report -> Production, Strapping, Additional Cost Added

--- ganapathy_pavers/ganapathy_pavers/report/itemwise_monthly_cw_production_report/itemwise_monthly_cw_production_report.py
+++ ganapathy_pavers/ganapathy_pavers/report/itemwise_monthly_cw_production_report/itemwise_monthly_cw_production_report.py
@@ -11,50 +11,28 @@ def execute(filters=None, _type=["Post", "Slab"]):
 	to_date = filters.get("to_date")
 
 	doc = frappe.get_all("CW Manufacturing", {"molding_date":["between", (from_date, to_date)],"type":["in",_type],"production_sqft":["!=",0],"docstatus":["!=",2]}, order_by = 'molding_date')
-	days_count = {}
-	data = {}
+	data = []
 
 	if doc:
-		labour_operator_cost=get_labour_operator_cost(doc)
-		for doc_name in doc:		
-			doc_details = frappe.get_doc("CW Manufacturing",doc_name.name)
-			for material in doc_details.item_details:
-				
-				if(f'{doc_details.molding_date.strftime("%B")}{material.item}.+.+.+.{material.parent}' not in days_count):days_count[f'{doc_details.molding_date.strftime("%B")}{material.item}.+.+.+.{material.parent}'] = 1
-				else:days_count[f'{doc_details.molding_date.strftime("%B")}{material.item}.+.+.+.{material.parent}'] += 1
-				data_key=f"""{material.item}----{doc_details.molding_date.strftime("%B")}"""
-				if data_key not in data:
-					data[data_key]={
-						"month": doc_details.molding_date.strftime("%B"),
-						"item": material.item,
-						"production_sqft": material.production_sqft,
-						"no_of_days": None,
-						"production_cost": None,
-						"expense": get_expense_cost(doc),
-						"labour_operator_cost": labour_operator_cost.get(data_key, {}).get("labour_operator_cost"),
-						"total_cost_per_sqft": None,
-					}
-				else:
-					data[data_key]["production_sqft"] += material.production_sqft
-
-		data=list(data.values())
-		
-	days_count=sorted(days_count, key=lambda x:x)
-	key = {i.split('.+.+.+.')[0]:0 for i in days_count}
-	for i in days_count:
-		key[i.split('.+.+.+.')[0]] += 1
-	for i in data:
-		i["no_of_days"] = key[i["month"]+i["item"]]
+		data = get_cw_cost(doc)
+	
+	for row in  data:
+		row['total_cost_per_sqft']=row.get("labour_operator_cost", 0)+row.get("prod_cost", 0)+row.get("strapping_cost", 0)+row.get("additional_cost", 0)
 	return columns, data
 
-def get_labour_operator_cost(doc_list):
+def get_cw_cost(doc_list):
 	doc_list=[i.name for i in doc_list]
 	query=f"""
 	SELECT
+		MONTHNAME(cw.molding_date) as month,
+		item.item,
+		sum(item.production_sqft) as production_sqft,
+		COUNT(item.item) as no_of_days,
 		CONCAT(item.item, '----', MONTHNAME(cw.molding_date)) as data_key,
-		count(item.item) as count,
-		SUM(item.production_sqft),
-		SUM(cw.total_labour_wages + cw.labour_expense_for_curing + cw.total_operator_wages)/SUM(cw.production_sqft)/2 as labour_operator_cost
+		SUM((cw.total_labour_wages + cw.labour_expense_for_curing + cw.total_operator_wages)*item.production_sqft/cw.production_sqft)/SUM(item.production_sqft)/2 as labour_operator_cost,
+		SUM(cw.raw_material_cost*item.production_sqft/cw.production_sqft)/SUM(item.production_sqft) as prod_cost,
+		AVG(strapping_cost_per_sqft) as strapping_cost,
+		AVG(additional_cost_per_sqft) as additional_cost
 	FROM `tabCW Manufacturing` cw
 	LEFT OUTER JOIN 
 		(
@@ -72,16 +50,10 @@ def get_labour_operator_cost(doc_list):
 	"""
 
 	res=frappe.db.sql(query, as_dict=True)
-	ret={}
-	for row in res:
-		ret[row.get("data_key")]=row
-	return ret
+	return res
 
 def get_expense_cost(doc_list):
-	pass
-
-def get_production_cost(doc_list):
-	pass
+	return {}
 
 def get_columns():
 
@@ -112,15 +84,27 @@ def get_columns():
 			"fieldtype":"Int"
 		},
 		{
-			"fieldname":"production_cost",
-			"label":_("Prod Cost"),
-			"width":120,
+			"fieldname":"prod_cost",
+			"label":_("Prod Cost(Raw Material)"),
+			"width":150,
 			"fieldtype":"Currency"
 		},
 		{
 			"fieldname":"labour_operator_cost",
 			"label":_("Labour and Operator Cost"),
 			"width":190,
+			"fieldtype":"Currency"
+		},
+		{
+			"fieldname":"strapping_cost",
+			"label":_("Strapping Cost"),
+			"width":120,
+			"fieldtype":"Currency"
+		},
+		{
+			"fieldname":"additional_cost",
+			"label":_("Additional Cost"),
+			"width":120,
 			"fieldtype":"Currency"
 		},
 		{
