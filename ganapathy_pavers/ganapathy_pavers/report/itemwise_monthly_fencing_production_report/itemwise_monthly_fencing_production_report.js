a27e4fd37c7104ec3aff20d3adc2d17b0e441b13
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2023-01-27 12:51:20 +0530

    Monthly default filters

--- ganapathy_pavers/ganapathy_pavers/report/itemwise_monthly_fencing_production_report/itemwise_monthly_fencing_production_report.js
+++ ganapathy_pavers/ganapathy_pavers/report/itemwise_monthly_fencing_production_report/itemwise_monthly_fencing_production_report.js
@@ -5,20 +5,20 @@
 frappe.query_reports["Itemwise Monthly Fencing Production Report"] = {
   filters: [
     {
-      fieldname: "from_date",
-      label: __("From Date"),
-      fieldtype: "Date",
-      width: "80",
-			default: frappe.datetime.month_start(),
-      reqd: 1,
+		fieldname: "from_date",
+		label: __("From Date"),
+		fieldtype: "Date",
+		width: "80",
+		default: frappe.datetime.month_start(),
+		reqd: 1,
     },
     {
-      fieldname: "to_date",
-      label: __("To Date"),
-      fieldtype: "Date",
-			default: frappe.datetime.month_end(),
-      width: "80",
-      reqd: 1,
+		fieldname: "to_date",
+		label: __("To Date"),
+		fieldtype: "Date",
+		default: frappe.datetime.month_end(),
+		width: "80",
+		reqd: 1,
     },
   ],
 };
