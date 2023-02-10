37bb1604a24803bd7ac877bb7a935d974f504b5e
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2023-02-10 15:57:08 +0530

    PI tds

--- ganapathy_pavers/custom/js/purchase_invoice.js
+++ ganapathy_pavers/custom/js/purchase_invoice.js
@@ -16,6 +16,7 @@ frappe.ui.form.on("Purchase Invoice", {
         if (frm.doc.branch) {
             frappe.db.get_value("Branch", frm.doc.branch, "is_accounting").then((value) => {
                 if (!value.message.is_accounting) {
+                    frm.set_value("apply_tds", 0);
                     if (frm.doc.taxes_and_charges) frm.set_value("taxes_and_charges", "");
                     if (frm.doc.tax_category) frm.set_value("tax_category", "");
                     if (frm.doc.taxes) frm.clear_table("taxes");
