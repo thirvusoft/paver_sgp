a19b6fc38115e44dcb8218c43ff9d9de6447917c
Nandhinidevi123 <nandhini.devi@thirvusoft.in>
2023-02-13 16:42:40 +0530

    change project name in title case

--- ganapathy_pavers/custom/py/site_work.py
+++ ganapathy_pavers/custom/py/site_work.py
@@ -80,7 +80,9 @@ def add_total_amount(items):
 
 
 def autoname(self, event):
-    self.name= self.project_name
+    self.name= self.project_name.title()
+    print("hrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
+    print(self.name)
 
 def create_status():
     print('Creating Property Setter for Site Work Status')
