78d6c196f9a5a3c30140718216d16c7004682499
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2023-02-13 07:28:01 +0000

    fix

--- ganapathy_pavers/utils/py/sales_invoice_printformat.py
+++ ganapathy_pavers/utils/py/sales_invoice_printformat.py
@@ -7,7 +7,7 @@ def sales_invoice_print_format(doc, field_name="item_name"):
         if row.get(field_name):
             _key=f"""{row.get(field_name)} {row.get("rate")}  {row.get("uom")}"""
             if _key not in data:
-                data[_key]={
+                row.update({
                 "item_name": row.get("item_name"),
                 field_name: row.get(field_name),
                 "uom": row.get("uom"),
@@ -15,14 +15,15 @@ def sales_invoice_print_format(doc, field_name="item_name"):
                 "hsn_code":row.get("gst_hsn_code"),
                 "amount": row.get("amount"),
                 "qty": row.get("qty")
-            }
+            })
+                data[_key]=row
             else:
                 data[_key]["amount"] += row.get("amount")
                 data[_key]["qty"] += row.get("qty")
         else:
             _key=f"""{row.get("item_name")} {row.get("rate")}  {row.get("uom")}"""
             if _key not in data:
-                data[_key]={
+                row.update({
                 "item_name": row.get("item_name"),
                 field_name: row.get(field_name),
                 "uom": row.get("uom"),
@@ -30,7 +31,8 @@ def sales_invoice_print_format(doc, field_name="item_name"):
                 "hsn_code":row.get("gst_hsn_code"),
                 "amount": row.get("amount"),
                 "qty": row.get("qty")
-            }
+            })
+                data[_key]=row
             else:
                 data[_key]["amount"] += row.get("amount")
                 data[_key]["qty"] += row.get("qty")
