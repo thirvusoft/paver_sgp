5a877da2c99973323b53357198ef23cf21fba71a
Nandhinidevi123 <nandhini.devi@thirvusoft.in>
2023-02-14 16:05:09 +0530

    Running Order Report

---
+++ ganapathy_pavers/ganapathy_pavers/report/site_work_completion___report/running_order_report/running_order_report.js
@@ -0,0 +1,44 @@
+// Copyright (c) 2023, Thirvusoft and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Running Order Report"] = {
+    "filters": [
+        {
+            "fieldname": "site_name",
+            "label": __("Site Name"),
+            "fieldtype": "Link",
+            "options": "Project",
+            "get_query": function () {
+                var customer = frappe.query_report.get_filter_value('customer');
+                return {
+                    filters: {
+                        "customer": customer
+                    }
+                };
+            },
+        },
+        {
+            "fieldname": "customer",
+            "label": __("Customer"),
+            "fieldtype": "Link",
+            "options": "Customer",
+
+        },
+        {
+            "fieldname": "type",
+            "label": __("Type"),
+            "fieldtype": "Select",
+            "default": "Pavers",
+            "options": "Pavers\nCompound Wall"
+        },
+        {
+            "fieldname": "status",
+            "label": __("Status"),
+            "fieldtype": "Select",
+
+            'options': frappe.get_meta('Project').fields.filter(df => df.fieldname === 'status')[0].options,
+            'default': frappe.get_meta('Project').fields.filter(df => df.fieldname === 'status')[0].options.includes('Open')
+        }
+    ]
+};
