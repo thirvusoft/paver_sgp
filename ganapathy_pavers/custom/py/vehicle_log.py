a230a6a4fd0f8bc0fd76fefc1b67aff9671fe2de
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2023-04-06 16:26:14 +0530

    Revert "vehicle seperate fastag credit acc"
    This reverts commit f27e6f43ecf05f20766293a811a4d018fc03e8ce.

--- ganapathy_pavers/custom/py/vehicle_log.py
+++ ganapathy_pavers/custom/py/vehicle_log.py
@@ -385,9 +385,9 @@ def fastag_expense(self, event=None):
     else:
         credit_acc = self.credit_account
         if not credit_acc:
-            credit_acc=frappe.db.get_value("Vehicle", self.license_plate, "fastag_credit_acc")
+            credit_acc=frappe.db.get_single_value("Vehicle Settings", "credit_account")
         if not credit_acc:
-            frappe.throw(f"""Please enter <b>Fastag Credit Account</b> in Vehicle""")        
+            frappe.throw(f"""Please enter <b>Credit Account</b>""")        
     
     doc=frappe.new_doc("Journal Entry")
     doc.update({
