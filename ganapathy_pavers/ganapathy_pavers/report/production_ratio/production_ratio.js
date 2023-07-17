048cba3cff42a8528f0f08db289bbbfdb0f35c62
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2023-07-17 13:42:37 +0530

    manufacturing: production ratio dashboard

---
+++ ganapathy_pavers/ganapathy_pavers/report/production_ratio/production_ratio.js
@@ -0,0 +1,55 @@
+// Copyright (c) 2023, Thirvusoft and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Production Ratio"] = {
+	filters: [
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.month_start(),
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.month_end(),
+		},
+		{
+			fieldname: 'paver_type',
+			label: 'Paver Type',
+			fieldtype: 'Link',
+			options: 'Paver Type',
+		},
+		{
+			fieldname: 'only_shot_blast',
+			label: 'Show Only Shot Blast',
+			fieldtype: 'Check'
+		},
+		{
+			fieldname: 'only_sample',
+			label: 'Show Only Sample',
+			fieldtype: 'Check'
+		},
+		{
+			"fieldname": "machine",
+			"label": __("Machine"),
+			"fieldtype": "MultiSelectList",
+			"options": "Workstation",
+			default: [],
+			get_data: async function (txt) {
+				let ws = (await frappe.db.get_list("Material Manufacturing", { fields: ["work_station"], limit: 0 }))
+				let machines = []
+				ws.forEach(data => {
+					if (!machines.includes(data.work_station)) {
+						machines.push(data.work_station)
+					}
+				});
+				return frappe.db.get_link_options('Workstation', txt, {
+					name: ["in", machines]
+				});
+			}
+		},
+	]
+};
