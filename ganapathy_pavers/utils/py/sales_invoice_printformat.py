31c39e6c8eefde876a5e0165735bd6b96f859bbb
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2023-09-28 06:24:37 +0000

    SI print

--- ganapathy_pavers/utils/py/sales_invoice_printformat.py
+++ ganapathy_pavers/utils/py/sales_invoice_printformat.py
@@ -1,20 +1,24 @@
 
 import frappe
+import ganapathy_pavers
 
 def sales_invoice_print_format(doc, field_name="item_code"):
     data={}
-    conv = 1
-    if doc.get('running_meter'):
-        conv = 19.679411764705883
 
     for row in doc.items:
+        conv = 1
+        uom = row.get('uom')
+        if doc.get('running_meter') and doc.get('running_meter_uom') and (item_meter_conv:=ganapathy_pavers.uom_conversion(item=row.get('item_code'), from_uom=row.get('uom'), from_qty=1, to_uom=doc.get('running_meter_uom'), throw_err=False)):
+            conv = 1/item_meter_conv
+            uom = doc.get('running_meter_uom')
+
         if row.get(field_name):
-            _key=f"""{row.get(field_name)} {row.get("rate")}  {row.get("uom")}"""
+            _key=f"""{row.get(field_name)} {row.get("rate")}  {uom}"""
             if _key not in data:
                 row.update({
                 "item_name": row.get("item_name"),
                 field_name: row.get(field_name),
-                "uom": row.get("uom"),
+                "uom": uom,
                 "rate": row.get("rate") * conv,
                 "hsn_code":row.get("gst_hsn_code"),
                 "amount": row.get("amount"),
@@ -25,12 +29,12 @@ def sales_invoice_print_format(doc, field_name="item_code"):
                 data[_key].amount += row.get("amount")
                 data[_key].qty += row.get("qty") / conv
         else:
-            _key=f"""{row.get("item_name")} {row.get("rate")}  {row.get("uom")}"""
+            _key=f"""{row.get("item_name")} {row.get("rate")}  {uom}"""
             if _key not in data:
                 row.update({
                 "item_name": row.get("item_name"),
                 field_name: row.get(field_name),
-                "uom": row.get("uom"),
+                "uom": uom,
                 "rate": row.get("rate") * conv,
                 "hsn_code":row.get("gst_hsn_code"),
                 "amount": row.get("amount"),
