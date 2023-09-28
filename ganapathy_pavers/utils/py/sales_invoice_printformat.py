f5ca4113a2433ad5c6811b701299aaef2d507c83
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2023-09-28 05:44:57 +0000

    fix

--- ganapathy_pavers/utils/py/sales_invoice_printformat.py
+++ ganapathy_pavers/utils/py/sales_invoice_printformat.py
@@ -3,6 +3,10 @@ import frappe
 
 def sales_invoice_print_format(doc, field_name="item_code"):
     data={}
+    conv = 1
+    if doc.get('running_meter'):
+        conv = 19.679411764705883
+
     for row in doc.items:
         if row.get(field_name):
             _key=f"""{row.get(field_name)} {row.get("rate")}  {row.get("uom")}"""
@@ -11,15 +15,15 @@ def sales_invoice_print_format(doc, field_name="item_code"):
                 "item_name": row.get("item_name"),
                 field_name: row.get(field_name),
                 "uom": row.get("uom"),
-                "rate": row.get("rate"),
+                "rate": row.get("rate") * conv,
                 "hsn_code":row.get("gst_hsn_code"),
                 "amount": row.get("amount"),
-                "qty": row.get("qty")
+                "qty": row.get("qty") / conv
             })
                 data[_key]=row
             else:
                 data[_key].amount += row.get("amount")
-                data[_key].qty += row.get("qty")
+                data[_key].qty += row.get("qty") / conv
         else:
             _key=f"""{row.get("item_name")} {row.get("rate")}  {row.get("uom")}"""
             if _key not in data:
@@ -27,15 +31,15 @@ def sales_invoice_print_format(doc, field_name="item_code"):
                 "item_name": row.get("item_name"),
                 field_name: row.get(field_name),
                 "uom": row.get("uom"),
-                "rate": row.get("rate"),
+                "rate": row.get("rate") * conv,
                 "hsn_code":row.get("gst_hsn_code"),
                 "amount": row.get("amount"),
-                "qty": row.get("qty")
+                "qty": row.get("qty") / conv
             })
                 data[_key]=row
             else:
                 data[_key].amount += row.get("amount")
-                data[_key].qty += row.get("qty")
+                data[_key].qty += row.get("qty") / conv
 
     data=list(data.values())
     return data
