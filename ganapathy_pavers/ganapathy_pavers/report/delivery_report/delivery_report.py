df3f6393936961955b77dadb2fd74e6a2519ea77
Nandhinidevi123 <nandhini.devi@thirvusoft.in>
2023-10-03 17:07:22 +0530

    change dont show status in delivery note

--- ganapathy_pavers/ganapathy_pavers/report/delivery_report/delivery_report.py
+++ ganapathy_pavers/ganapathy_pavers/report/delivery_report/delivery_report.py
@@ -42,7 +42,7 @@ class PartyLedgerSummaryReport(object):
 
 			if self.filters.get("sw_status") and row.get("project"):
 				try:
-					if (frappe.get_value("Project", row.get("project"), "status") not in self.filters.get("sw_status")):
+					if (frappe.get_value("Project", row.get("project"), "status") in self.filters.get("sw_status")):
 						final_data.append(row)
 				except:
 					pass
