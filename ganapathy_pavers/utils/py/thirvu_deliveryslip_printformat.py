30f5fe8b3c6053a9edc82354cba051c9f37cd418
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2023-10-20 18:34:03 +0530

    delivered qty in delivery slip

--- ganapathy_pavers/utils/py/thirvu_deliveryslip_printformat.py
+++ ganapathy_pavers/utils/py/thirvu_deliveryslip_printformat.py
@@ -1,18 +1,48 @@
 import frappe
 
 def print_format(docname, doctype='Delivery Note Item'):
+    posting_date, posting_time = frappe.db.get_value('Delivery Note', docname, ['posting_date', 'posting_time'])
     same_item=  frappe.db.sql(f"""
                             select 
                                 dn_item.item_name, 
                                 sum(dn_item.ts_qty) as Bdl, 
                                 sum(dn_item.pieces) as pieces, 
-                                count(dn_item.item_code) as count 
+                                count(dn_item.item_code) as count,
+                                case
+                                    when ifnull(dn_item.against_sales_order, '') != ''
+                                        then (
+                                            select
+                                                sum(soi.qty)
+                                            from `tabSales Order Item` soi
+                                            where 
+                                                soi.docstatus = 1 and
+                                                soi.parent = dn_item.against_sales_order and
+                                                soi.item_code = dn_item.item_code
+                                        )
+                                    else 0
+                                end as order_qty,
+                                case
+                                    when ifnull(dn_item.against_sales_order, '') != ''
+                                        then (
+                                            select
+                                                sum(dni.qty)
+                                            from `tabDelivery Note Item` dni
+                                            inner join `tabDelivery Note` dn on dn.name = dni.parent
+                                            where 
+                                                dn.docstatus = 1 and 
+                                                dni.docstatus = 1 and
+                                                dn.posting_date <= '{posting_date}' and
+                                                dn.posting_time <= '{posting_time}' and
+                                                dni.against_sales_order = dn_item.against_sales_order and
+                                                dni.item_code = dn_item.item_code
+                                        )
+                                    else 0
+                                end as delivered_qty
                             from `tab{doctype}` as dn_item 
                             where 
                                 parent='{docname}' 
                             group by 
                                 dn_item.item_code 
-                            having count(dn_item.item_code)>1 
                         """,as_dict=1)
     return same_item
 
