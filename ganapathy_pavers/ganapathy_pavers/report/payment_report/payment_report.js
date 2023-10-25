a9f8684ebb666e2d979874777ae7f24f3dda648a
HR-EMP-007 <chandana@thirvusoft.in>
2023-10-25 18:45:08 +0530

    sales and payment report

---
+++ ganapathy_pavers/ganapathy_pavers/report/payment_report/payment_report.js
@@ -0,0 +1,44 @@
+// Copyright (c) 2023, Thirvusoft and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Payment Report"] = {
+	"filters": [
+		{
+			fieldname: "party",
+			label: __("Party"),
+			fieldtype: "Select",
+			options: 'Customer\nSupplier',
+			default:"Customer",
+			reqd:1
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.month_start(),
+			reqd:1
+
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.month_end(),
+			reqd:1
+
+		},
+		{
+			fieldname: 'branch',
+			label: 'Branch',
+			fieldtype: 'Link',
+			options: 'Branch',
+		},
+		{
+			fieldname: 'site_work',
+			label: 'Site Work',
+			fieldtype: 'Link',
+			options: 'Project',
+		}
+	]
+};
---
+++ ganapathy_pavers/ganapathy_pavers/report/sales_report/sales_report.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2023, Thirvusoft and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Sales Report"] = {
+	"filters": [
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.month_start(),
+			reqd:1
+
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.month_end(),
+			reqd:1
+
+		},
+		{
+			fieldname: 'branch',
+			label: 'Branch',
+			fieldtype: 'Link',
+			options: 'Branch',
+		},
+		{
+			fieldname: 'site_work',
+			label: 'Site Work',
+			fieldtype: 'Link',
+			options: 'Project',
+		}
+	]
+};
