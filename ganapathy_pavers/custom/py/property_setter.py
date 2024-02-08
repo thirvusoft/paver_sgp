bc314f649d44dbe36612a16f23709185834d447e
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2024-02-08 10:49:12 +0000

    live changes

--- ganapathy_pavers/custom/py/property_setter.py
+++ ganapathy_pavers/custom/py/property_setter.py
@@ -8,7 +8,7 @@ def property_setter():
         'field_name' : 'type',
         'property': 'options',
         'property_type' : 'Select',
-        'value' : '\nCompound Wall\nPavers'
+        'value' : '\nCompound Wall\nPavers\nU Drain'
         
     })
     doc.save()
